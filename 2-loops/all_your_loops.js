9b49ac2f315df9d0a4c8d1c3fe2f67ce165fbaa7
Ben Mahowald <ben.mahowald@gmail.com>
2016-08-29 16:05:38 -0500

    all done

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -2,7 +2,16 @@
 // Each iteration will increase the value
 // of checkz by 1
 var checkz = 0;
+for (var i = 0; i < 5; i++) {
+  checkz += 1;
+}
 
 // 2 - Write a while loop that runs
 // 3 times. Each iteration will decrease
 // the value of checkz by -2
+checkz = 6;
+var i = 0;
+while (i < 3) {
+  i++;
+  checkz -= 2;
+}
--- 5-functions/func.js
+++ 5-functions/func.js
@@ -2,20 +2,38 @@
 // that takes no arguments and
 // returns the value 4
 
+var first = function () {
+  return 4;
+};
+
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
 
+var second = function (num1, num2) {
+  return num1 * num2;
+};
+
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+
+var third = function (num) {
+  console.log(num);
+};
 
 // 4 - Write a function declaration
 // that takes a number and divides it
 // by two.
 
+var fourth = function (number) {
+  return number / 2;
+};
+
 // 5 - Call the function from 4 and
 // pass it into the function from 2 called with
 // the arguments 10, 20. Console log the result.
+
+console.log(fourth(second(10, 20)));
